41e68529a94696a7f49d1343edf283b9b35ac3b6
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 23:38:28 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Multiply
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=10;i++)
+{
+System.out.println(n+"*"+i+"="+n*i);
+}
+}
+}
