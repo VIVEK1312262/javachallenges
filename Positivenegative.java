06fbf6afb4c846280531efa5126ce6fdb2f15305
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 15:33:44 +0530

    Add files via upload

---
+++ Positivenegative.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Positivenegative
+
+{
+
+public static void main(String s[])
+
+{
+
+Scanner sc=new Scanner(System.in);
+
+int number=sc.nextInt();
+
+if(number>0)
+
+System.out.println("POSITIVE");
+
+else
+if(number<0)
+
+System.out.println("NEGATIVE");
+
+else
+
+System.out.println("ZERO");
+
+}
+
+}
