bdc509942acdce673a217eee2dd0aa2144d97d23
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 16:12:14 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Vowels
+{
+
+public static void main(String s[])
+
+{
+
+char c;
+Scanner sc=new Scanner(System.in);
+
+c=sc.next().charAt(0);
+
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+
+System.out.println("VOWEL");
+
+else
+
+
+System.out.println("CONSONANT");
+
+
+}
+
+}
