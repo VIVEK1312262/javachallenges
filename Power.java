02eda3a40db7e4172e1a668fb63231530ef14179
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 21:13:34 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Power
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+double num=Math.pow(n1,n2);
+System.out.println(num);
+}
+}
