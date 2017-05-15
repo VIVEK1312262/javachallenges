4075fc23f31f3e4a61782b33808c7ad44a207c2b
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 15:40:29 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Oddeven
+{
+
+public static void main(String s[])
+
+{
+
+Scanner sc=new Scanner(System.in);
+
+int number=sc.nextInt();
+
+if(number%2==0)
+
+System.out.println("even");
+
+else
+
+
+System.out.println("odd");
+
+
+}
+
+}
