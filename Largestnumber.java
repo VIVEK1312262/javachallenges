5793b725da254d26acf6b481105f70420bc7a976
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 16:35:13 +0530

    Add files via upload

---
+++ Largestnumber.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Largestnumber
+{
+
+public static void main(String s[])
+
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+
+a=sc.nextInt();
+
+
+b=sc.nextInt();
+
+c=sc.nextInt();
+if(a>b&&a>c)
+System.out.println("the number"+a+"is largest");
+
+else
+
+if(b>a&&b>c)
+System.out.println("the number"+b+"is largest");
+
+else
+System.out.println("the number"+c+"is largest");
+
+
+}
+
+}
