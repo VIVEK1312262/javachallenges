824c24101633d3f4e325d6e0b221b4de45dc472d
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 16:25:37 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Alphabet
+{
+
+public static void main(String s[])
+
+{
+
+char c;
+Scanner sc=new Scanner(System.in);
+
+c=sc.next().charAt(0);
+
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+
+System.out.println("alphabet");
+
+else
+
+
+System.out.println("not alphabet");
+
+
+}
+
+}
