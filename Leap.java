8513a11b86b305e5ad2195e9749af2dade1ed413
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-15 16:47:19 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Leap
+{
+
+public static void main(String s[])
+
+{
+int n;
+Scanner sc=new Scanner(System.in);
+
+n=sc.nextInt();
+if((n%4==0)&&(n%100!=0))
+System.out.println("this is leap year");
+
+else
+
+
+if(n%400==0)
+System.out.println("this is leap year");
+
+else
+if(n%100==0)
+System.out.println("this is not leap year");
+
+else
+System.out.println("this is not leap year");
+
+}
+
+}
