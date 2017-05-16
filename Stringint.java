08a09e8cb3f344b6ad82f13e567b782d8c373862
VIVEK1312262 <viveksakthivel262@gmail.com>
2017-05-16 09:17:51 +0530

    Add files via upload

---
+++ Stringint.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Stringint
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+int s2=Integer.parseInt(s1);
+System.out.println(s2);
+}
+}
